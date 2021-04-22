ad9a26cb6124a8380b106465487178031e8836ee
Анастасия Брикун <a.brikun@vipcg.ru>
2021-04-22 18:43:49 +0300

    App init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  .wrapper
+    header
+      .navbar
+          .container
+            .navbar-content
+              router-link.header-logo(
+                to="/"
+              ) Film library
+              .button-burger(
+                @click="menuShow = !menuShow"
+                :class="{ active: menuShow}"
+              )
+                span.line.line-1
+                span.line.line-2
+                span.line.line-3
+              .navbar-list__wrapper(
+                :class="{ active: menuShow}"
+              )
+                ul.navbar-list
+                  li.navbar-item(
+                    v-for="link in linkMenu"
+                    :key="link.title"
+                    @click="menuShow = false"
+                  )
+                    router-link.navbar-link(
+                      :to="`${link.url}`"
+                    ) {{ link.title }}
+    router-view
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuShow: false,
+      linkMenu: [
+        {title: 'Home', url: '/'},
+        {title: 'Login', url: '/login'},
+        {title: 'Registration', url: '/registration'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../node_modules/uimini/src/stylus/main.styl'
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .content-wrapper
+    section
+        .container
+            h1.ui-title-1 Home
+    section
+      .container
+        .task-list
+          .task-item(
+
+          )
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tasks: [
+        {
+          'id': 1,
+          'title': 'GrowthBusters: Hooked on Growth',
+          'description': 'I directed this documentary challenging the myths linking growth with prosperity and fulfillment. It explores how our beliefs about economic and consumption',
+          'whatWatch': 'Film',
+          'completed': false,
+          'editing': false
+        },
+        {
+          'id': 2,
+          'title': 'Game of thrones',
+          'description': 'Best serials',
+          'whatWatch': 'Serial',
+          'completed': false,
+          'editing': false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped >
+
+</style>
